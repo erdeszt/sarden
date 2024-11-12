21620e0c91661bb8e8141e4ec742112410b33c72
erdeszt <erdeszt@users.noreply.github.com>
2024-11-13 00:10:54 +0100

    Add upickle instances for some temporal types

--- core/src/main/scala/org/sarden/core/domain/todo/model.scala
+++ core/src/main/scala/org/sarden/core/domain/todo/model.scala
@@ -1,10 +1,12 @@
 package org.sarden.core.domain.todo
 
 import java.time.{DayOfWeek, LocalTime, OffsetDateTime}
+import java.util.concurrent.TimeUnit
 
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration.{FiniteDuration, TimeUnit}
 
 import com.github.f4b6a3.ulid.Ulid
+import upickle.default.{ReadWriter, readwriter}
 
 opaque type TodoId = Ulid
 
@@ -23,7 +25,52 @@ case class CreateTodo(
     schedule: Schedule,
 )
 
-enum Schedule:
+given ReadWriter[LocalTime] = readwriter[ujson.Value].bimap[LocalTime](
+  value => ujson.Obj(("hour", value.getHour), ("minute", value.getMinute)),
+  json => LocalTime.of(json("hour").num.toInt, json("minute").num.toInt),
+)
+
+given CanEqual[TimeUnit, TimeUnit] = CanEqual.derived
+
+class InvalidTimeUnitException(rawValue: String)
+    extends Exception(s"Invalid TimeUnit: `${rawValue}`")
+
+object TimeUnitPickle:
+  def write: TimeUnit => String = {
+    case TimeUnit.NANOSECONDS  => "NANOSECONDS"
+    case TimeUnit.MICROSECONDS => "MICROSECONDS"
+    case TimeUnit.MILLISECONDS => "MILLISECONDS"
+    case TimeUnit.SECONDS      => "SECONDS"
+    case TimeUnit.MINUTES      => "MINUTES"
+    case TimeUnit.HOURS        => "HOURS"
+    case TimeUnit.DAYS         => "DAYS"
+  }
+
+  def read: String => Either[InvalidTimeUnitException, TimeUnit] = {
+    case "NANOSECONDS"  => Right(TimeUnit.NANOSECONDS)
+    case "MICROSECONDS" => Right(TimeUnit.MICROSECONDS)
+    case "MILLISECONDS" => Right(TimeUnit.MILLISECONDS)
+    case "SECONDS"      => Right(TimeUnit.SECONDS)
+    case "MINUTES"      => Right(TimeUnit.MINUTES)
+    case "HOURS"        => Right(TimeUnit.HOURS)
+    case "DAYS"         => Right(TimeUnit.DAYS)
+    case other          => Left(InvalidTimeUnitException(other))
+  }
+
+given ReadWriter[FiniteDuration] =
+  readwriter[ujson.Value].bimap[FiniteDuration](
+    value =>
+      ujson.Obj(
+        ("length", value.length),
+        ("unit", TimeUnitPickle.write(value.unit)),
+      ),
+    json =>
+      TimeUnitPickle.read(json("unit").str) match
+        case Left(error) => throw error
+        case Right(unit) => FiniteDuration(json("length").num.toInt, unit),
+  )
+
+enum Schedule derives ReadWriter:
   case EverySecondFridayOfTheMonth(timeOfDay: LocalTime)
 
   private given CanEqual[DayOfWeek, DayOfWeek] =

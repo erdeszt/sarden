82031e3cb8ad73eed7951d61c7ca5c04f06cd7e0
erdeszt <erdeszt@users.noreply.github.com>
2025-02-09 13:11:54 +0100

    Move repo instances into dto file

--- core/src/main/scala/org/sarden/core/todo/internal/dto.scala
+++ core/src/main/scala/org/sarden/core/todo/internal/dto.scala
@@ -1,6 +1,11 @@
 package org.sarden.core.todo.internal
 
 import doobie.Read
+import io.scalaland.chimney.partial.Result
+import io.scalaland.chimney.{PartialTransformer, Transformer}
+import zio.json.*
+
+import org.sarden.core.todo.TodoSchedule
 
 private[internal] case class TodoDTO(
     id: String,
@@ -10,3 +15,10 @@ private[internal] case class TodoDTO(
     lastRun: Option[Long],
     createdAt: Long,
 ) derives Read
+
+given JsonCodec[TodoSchedule] = JsonCodec.derived
+
+given PartialTransformer[String, TodoSchedule] = PartialTransformer: raw =>
+  Result.fromEitherString(raw.fromJson)
+
+given Transformer[TodoSchedule, String] = _.toJson

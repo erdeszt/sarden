f4759c07ff09c0b2e09bd478d245256cdb64cac6
erdeszt <erdeszt@users.noreply.github.com>
2025-02-09 13:12:48 +0100

    Fix tranzactio dependency

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val scalaTags = "0.13.1"
   val sqlite = "3.47.0.0"
   val tapir = "1.11.9"
-  val tranzactio = "5.3.0-M3-LOCAL-1"
+  val tranzactio = "5.3.0-M2"
   val ulid = "5.2.3"
   val zio = "2.1.14"
   val zioJson = "0.7.14"
